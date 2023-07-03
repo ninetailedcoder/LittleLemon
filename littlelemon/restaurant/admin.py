bfe8bf2c48020694920a040eabd543a778168c55
Tory DeMaio <torydemaio@gmail.com>
2023-07-02 20:19:33 -0500

    menu and booking models

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import menu,booking
 # Register your models here.
+admin.site.register(menu)
+admin.site.register(booking)
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.FloatField(max_length=10)
+    inventory = models.IntegerField(max_length=5)
+
+    def __str__(self):
+        return self.title
+    
+class booking(models.Model):
+    name = models.CharField(max_length=255)
+    num = models.IntegerField()
+    bookingDate = models.DateTimeField()
+    def __str__(self):
+        return self.name
